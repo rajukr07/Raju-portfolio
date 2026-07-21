6733f4ca5ee05c5190d2f13c18755bd570aa371d
Raju Kumar <rajukumarsdr95@gmail.com>
2026-07-21 08:00:48 +0530

    update skills section

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,18 @@
 export const skills = [
   {
     category: "Frontend",
-    items: ["React.js", "JavaScript", "HTML5", "CSS3"],
+    items: ["React.js","React Native","JavaScript","Typescript","HTML5", "CSS3",],
   },
   {
     category: "Backend",
-    items: ["Node.js", "Express.js", "REST APIs", "JWT Authentication"],
+    items: ["Node.js", "Express.js", "REST APIs", "JWT Authentication",],
   },
   {
     category: "Databases",
-    items: ["MongoDB", "MySQL", "PostgreSQL"],
+    items: ["MongoDB", "MySQL", "PostgreSQL",],
   },
   {
     category: "Tools",
-    items: ["Git", "GitHub", "Postman", "Swagger", "Docker", "VS Code"],
+    items: ["Git", "GitHub", "Postman", "Swagger", "Docker", "VS Code",],
   },
 ];
