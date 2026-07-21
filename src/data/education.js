52c428bfec745f621866eea4edbfc1b3214ff58c
Raju Kumar <rajukumarsdr95@gmail.com>
2026-07-21 07:58:14 +0530

    Update education section

--- src/data/education.js
+++ src/data/education.js
@@ -1,20 +1,20 @@
 export const education = [
   {
-    course: "B.Tech in Computer Science Engineering",
-    institute: "J.C. Bose University of Science and Technology, YMCA",
+    course: "B.Tech in Computer Science & Engineering.",
+    institute: "J.C. Bose University of Science and Technology, YMCA ,Faridabad, Haryana",
     duration: "2022 - 2025",
     score: "7.5 CGPA",
   },
   {
-    course: "Diploma",
-    institute: "Government Polytechnic Barauni",
+    course: "Diploma in Computer Science & Engineering.",
+    institute: "Government Polytechnic Barauni,Begusarai, Bihar",
     duration: "2018 - 2021",
     score: "7.8 CGPA",
   },
   {
-    course: "10+2",
-    institute: "RK JS High School, Naulagarh, Begusarai, Bihar",
-    duration: "",
+    course: "Secondary Education",
+    institute: "RK JS High School, Naulagarh, Begusarai, Bihar.",
+    duration: "2016-18",
     score: "70%",
   },
 ];
